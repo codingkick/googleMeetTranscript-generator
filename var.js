70ee5b889961cd94b75d7cc1fefcf450a270e5af
codingkick <57174101+codingkick@users.noreply.github.com>
2021-12-22 06:55:34 +0530

    Delete var.js

--- var.js
+++
@@ -1,6 +0,0 @@
-export const API_KEY = "AIzaSyCG1FNQ7OVymxvgy_CVILlctpzJuc3JJSk";
-export const AUTH_DOMAIN = "transcript-generator-6fe0c.firebaseapp.com";
-export const PROJECT_ID = "transcript-generator-6fe0c";
-export const STORAGE_BUCKET = "transcript-generator-6fe0c.appspot.com";
-export const MESSAGING_SENDER_ID = "268654169176";
-export const APP_ID = "1:268654169176:web:85498ade864b40a50bce2d";
